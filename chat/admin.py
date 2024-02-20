720516d8449498a3ed823c95631fdfca1e5bd5fa
Zwogli <Mathiaskohler@web.de>
2024-02-20 08:25:21 +0100

    Create first model in django

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from chat.models import Message
+
+# Definiert die Anzeige im Dashboard
+class MessageAdmin(admin.ModelAdmin):    
+    fields = ('text','created_at', 'author', 'receiver')    
+    list_display = ('created_at', 'author', 'text', 'receiver')    
+    search_fields = ('text',)
+
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
+from datetime import date
 from django.db import models
 
+
 # Create your models here.
+
+class Chat(models.Model):    
+    created_at = models.DateField(default=date.today)
+
+class Message(models.Model):
+    text = models.CharField(max_length=500)    
+    created_at = models.DateField(default=date.today)    
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)    
+    author = models.CharField(max_length=50)    
+    receiver = models.CharField(max_length=50)
