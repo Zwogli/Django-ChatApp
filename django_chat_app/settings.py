4d1ca687d62e30eea5308866730010d9b9c11a51
Zwogli <Mathiaskohler@web.de>
2024-02-21 09:36:33 +0100

    Add mastertemplate for all urls

--- django_chat_app/settings.py
+++ django_chat_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'django_chat_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')         #importiert die Daten aus dem templates Ordner
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
