3b1df41dd54f4dccbb04eda4c38265f1e3009985
Zwogli <Mathiaskohler@web.de>
2024-02-26 22:39:25 +0100

    Add new file

---
+++ static/js/message.js
@@ -0,0 +1,30 @@
+// async function sendMessage() {
+// 	let form = new FormData();
+// 	let djangoToken = "{{ csrf_token }}";
+// 	form.append("messagefield", messagefield.value); // append = anhängen
+// 	form.append("csrfmiddlewaretoken", djangoToken);
+
+// 	try {
+// 		messageContainer.innerHTML += `
+//         <div id="messagePreview">
+//             <span class="color-gray">[Datum]</span>
+//             <i class="color-gray">{{ request.user.first_name }}:</i> <div class="color-gray">${messagefield.value}</div>
+//         </div>
+//         `;
+// 		await fetch("/chat/", {
+// 			method: "POST",
+// 			body: form,
+// 		});
+
+// 		document.getElementById("messagePreview").remove();
+// 		messageContainer.innerHTML += `
+//         <div>
+//             <span class="color-gray">[Datum]</span>
+//             <i>{{ request.user.first_name }}:</i> <div>${messagefield.value}</div>
+//         </div>
+//         `;
+// 		console.log("Send message succes!");
+// 	} catch (e) {
+// 		console.error("FAIL send message!", e);
+// 	}
+// }
