348c451ca3b73cacbb8d5e3edb8002d1e4fb8224
Zwogli <Mathiaskohler@web.de>
2024-03-02 09:51:21 +0100

    Test outsource js/chat

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,3 +1,4 @@
+// {{ request.user.first_name }}
 async function sendMessage(user) {
 	let form = new FormData();
 	let djangoToken = "{{ csrf_token }}";
@@ -11,19 +12,19 @@ async function sendMessage(user) {
             <i class="color-gray">${user}:</i> <div class="color-gray">${messagefield.value}</div>
         </div>
         `;
-		debugger;
-		await fetch("/chat/", {
-			method: "POST",
-			body: form,
-		});
+		// debugger;
+		// await fetch("/chat/", {
+		// 	method: "POST",
+		// 	body: form,
+		// });
 
-		document.getElementById("messagePreview").remove();
-		messageContainer.innerHTML += `
-		<div>
-		    <span class="color-gray">[Datum]</span>
-		    <i>{{ request.user.first_name }}:</i> <div>${messagefield.value}</div>
-		</div>
-		`;
+		// document.getElementById("messagePreview").remove();
+		// messageContainer.innerHTML += `
+		// <div>
+		//     <span class="color-gray">[Datum]</span>
+		//     <i>{{ request.user.first_name }}:</i> <div>${messagefield.value}</div>
+		// </div>
+		// `;
 		console.log("Send message succes!");
 	} catch (e) {
 		console.error("FAIL send message!", e);
