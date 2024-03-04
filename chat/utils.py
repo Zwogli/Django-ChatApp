c5347631828ebc70d63906d56e80952bffabdb71
Zwogli <Mathiaskohler@web.de>
2024-03-04 11:22:18 +0100

    Rework correct datetime for json

--- chat/utils.py
+++ chat/utils.py
@@ -7,12 +7,16 @@ def isRequestPost(request):
 
 
 def serializeJson(request, object):
+    # Convert dateobject to datetime-Objekt
+    created_at_datetime = object.created_at
+    # Convert dateobject to desired format
+    formatted_created_at = created_at_datetime.strftime('%B %d, %Y')
     #Creates JSON-Object
     return {
             'pk': object.pk,
             'fields': {
                 'text': object.text,
-                'created_at': object.created_at,
+                'created_at': formatted_created_at,
                 'chat': object.chat.id,
                 'author': object.author.id,
                 'receiver': object.receiver.id,
